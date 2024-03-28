cb38629114352654ffea18efd0825cde271a1263
Meerata <merata87@gmail.com>
2024-03-28 10:54:40 +0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // Make a fetch request to the Game of Thrones API
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json()) // Convert the response to JSON
+    .then((json) => renderBooks(json)); // Pass the JSON data to the renderBooks function
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+document.addEventListener("DOMContentLoaded", function () {
+  fetchBooks(); // Call fetchBooks when the DOM content is loaded
 });
